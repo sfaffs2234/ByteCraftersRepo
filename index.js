57c2e52aa70abd0ff9ccad1c7e7ea1cc2d5049b3
sfaffs2234 <js1adonarig@hotmail.com>
2024-05-11 09:54:26 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
+const shellSort = (arr) => {
+  const n = arr.length;
+  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    for (let i = gap; i < n; i++) {
+      const temp = arr[i];
+      let j;
+      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+        arr[j] = arr[j - gap];
+      }
+      arr[j] = temp;
     }
   }
-  return dp[0];
-}
+  return arr;
+};
