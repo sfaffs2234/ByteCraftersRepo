4bf1fd60521480144892f22bfa34163fa14b6176
sfaffs2234 <js1adonarig@hotmail.com>
2024-05-15 04:45:05 +0700

    add more features

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-function uniquePathsWithObstacles(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp = Array.from(Array(m), () => Array(n).fill(0));
-  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (obstacleGrid[i][j] === 1) continue;
-      if (i > 0) dp[i][j] += dp[i - 1][j];
-      if (j > 0) dp[i][j] += dp[i][j - 1];
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+      let low = i + 1;
+      let high = nums.length - 1;
+      let sum = 0 - nums[i];
+      while (low < high) {
+        if (nums[low] + nums[high] === sum) {
+          result.push([nums[i], nums[low], nums[high]]);
+          while (low < high && nums[low] === nums[low + 1]) low++;
+          while (low < high && nums[high] === nums[high - 1]) high--;
+          low++;
+          high--;
+        } else if (nums[low] + nums[high] < sum) low++;
+        else high--;
+      }
     }
   }
-  return dp[m - 1][n - 1];
+  return result;
 }
