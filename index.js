a9a03b3351a9e8f9631b22aca9f18e8cacff2ea1
sfaffs2234 <js1adonarig@hotmail.com>
2024-05-16 21:25:20 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function mergeKLists(lists) {
-  if (lists.length === 0) return null;
-  while (lists.length > 1) {
-    const first = lists.shift();
-    const second = lists.shift();
-    const merged = mergeTwoLists(first, second);
-    lists.push(merged);
-  }
-  return lists[0];
-}
+const titleCase = (str) =>
+  str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
